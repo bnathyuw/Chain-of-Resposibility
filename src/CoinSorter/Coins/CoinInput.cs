7f96e5a29f82cf7490b74e6c7235a9df677eef6c
Matthew Butt <matthew.butt@7digital.com>
2012-03-09 15:15:13 +0000

    Added coin diameters

--- src/CoinSorter/Coins/CoinInput.cs
+++ src/CoinSorter/Coins/CoinInput.cs
@@ -6,27 +6,27 @@ namespace CoinSorter.Coins
 		public double Mass { get; set; }
 
 		public static CoinInput OnePenny() {
-			return new CoinInput { Mass = 3.56 };
+			return new CoinInput { Mass = 3.56, Diameter = 20.32 };
 		}
 
 		public static CoinInput TwoPence() {
-			return new CoinInput { Mass = 7.12 };
+			return new CoinInput { Mass = 7.12, Diameter = 25.9 };
 		}
 
 		public static CoinInput FivePence() {
-			return new CoinInput { Mass = 3.25 };
+			return new CoinInput { Mass = 3.25, Diameter = 18 };
 		}
 
 		public static CoinInput TenPence() {
-			return new CoinInput { Mass = 6.5 };
+			return new CoinInput { Mass = 6.5, Diameter = 24.5 };
 		}
 
 		public static CoinInput TwentyPence() {
-			return new CoinInput { Mass = 5 };
+			return new CoinInput {Mass = 5, Diameter = 22.5};
 		}
 
 		public static CoinInput FiftyPence() {
-			return new CoinInput{Mass = 8};
+			return new CoinInput {Mass = 8, Diameter = 27.3};
 		}
 	}
 }
