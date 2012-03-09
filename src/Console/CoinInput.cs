18b1d5f5e51f059389f1bb1daba823ea0b5e4080
Matthew Butt <matthew.butt@7digital.com>
2012-03-09 14:11:03 +0000

    And factory methods

--- src/Console/CoinInput.cs
+++ src/Console/CoinInput.cs
@@ -3,5 +3,29 @@ namespace Console
 	public class CoinInput
 	{
 		public double Mass { get; set; }
+
+		public static CoinInput OnePenny() {
+			return new CoinInput { Mass = 3.56 };
+		}
+
+		public static CoinInput TwoPence() {
+			return new CoinInput { Mass = 7.12 };
+		}
+
+		public static CoinInput FivePence() {
+			return new CoinInput { Mass = 3.25 };
+		}
+
+		public static CoinInput TenPence() {
+			return new CoinInput { Mass = 6.5 };
+		}
+
+		public static CoinInput TwentyPence() {
+			return new CoinInput { Mass = 5 };
+		}
+
+		public static CoinInput FiftyPence() {
+			return new CoinInput{Mass = 8};
+		}
 	}
 }
--- src/Console/StructureMap/CoinIdentifierRegistry.cs
+++ src/Console/StructureMap/CoinIdentifierRegistry.cs
@@ -8,27 +8,27 @@ namespace Console.StructureMap
 		public CoinIdentifierRegistry() {
 			For<ICoinIdentifier>().Add<RealCoinIdentifier>().Named("FiftyPence")
 				.Ctor<ICoinIdentifier>().Is(x => x.GetInstance<ICoinIdentifier>("TwentyPence"))
-				.Ctor<CoinInput>("coinSpec").Is(new CoinInput{Mass = 8})
+				.Ctor<CoinInput>("coinSpec").Is(CoinInput.FiftyPence())
 				.Ctor<string>("value").Is("50p");
 			For<ICoinIdentifier>().Add<RealCoinIdentifier>().Named("TwentyPence")
 				.Ctor<ICoinIdentifier>().Is(x => x.GetInstance<ICoinIdentifier>("TenPence"))
-				.Ctor<CoinInput>("coinSpec").Is(new CoinInput { Mass = 5 })
+				.Ctor<CoinInput>("coinSpec").Is(CoinInput.TwentyPence())
 				.Ctor<string>("value").Is("20p");
 			For<ICoinIdentifier>().Add<RealCoinIdentifier>().Named("TenPence")
 				.Ctor<ICoinIdentifier>().Is(x => x.GetInstance<ICoinIdentifier>("FivePence"))
-				.Ctor<CoinInput>("coinSpec").Is(new CoinInput { Mass = 6.5 })
+				.Ctor<CoinInput>("coinSpec").Is(CoinInput.TenPence())
 				.Ctor<string>("value").Is("10p");
 			For<ICoinIdentifier>().Add<RealCoinIdentifier>().Named("FivePence")
 				.Ctor<ICoinIdentifier>().Is(x => x.GetInstance<ICoinIdentifier>("TwoPence"))
-				.Ctor<CoinInput>("coinSpec").Is(new CoinInput { Mass = 3.25 })
+				.Ctor<CoinInput>("coinSpec").Is(CoinInput.FivePence())
 				.Ctor<string>("value").Is("5p");
 			For<ICoinIdentifier>().Add<RealCoinIdentifier>().Named("TwoPence")
 				.Ctor<ICoinIdentifier>().Is(x => x.GetInstance<ICoinIdentifier>("OnePenny"))
-				.Ctor<CoinInput>("coinSpec").Is(new CoinInput { Mass = 7.12 })
+				.Ctor<CoinInput>("coinSpec").Is(CoinInput.TwoPence())
 				.Ctor<string>("value").Is("2p");
 			For<ICoinIdentifier>().Add<RealCoinIdentifier>().Named("OnePenny")
 				.Ctor<ICoinIdentifier>().Is(x => x.GetInstance<ICoinIdentifier>("Fake"))
-				.Ctor<CoinInput>("coinSpec").Is(new CoinInput { Mass = 3.56 })
+				.Ctor<CoinInput>("coinSpec").Is(CoinInput.OnePenny())
 				.Ctor<string>("value").Is("1p");
 			For<ICoinIdentifier>().Add<FakeCoinIdentifier>().Named("Fake");
 		}
